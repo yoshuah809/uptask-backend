4ffaf42078e66e18d8fefcaa929544b47ff13c4e
JOsue Garcia <yoshuah809@gmail.com>
2023-02-28 05:21:47 -0600

    added hash password functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,15 @@
 import User from "../models/Usuario.js";
 
 const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    const error = new Error("User already exists");
+    return res.status(400).json({ msg: error.message });
+  }
+
   try {
     const user = new User(req.body);
     const savedUser = await user.save();
--- models/Usuario.js
+++ models/Usuario.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = mongoose.Schema(
   {
@@ -28,6 +29,14 @@ const userSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+userSchema.pre("save", async function (next) {
+  // Hash password
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
